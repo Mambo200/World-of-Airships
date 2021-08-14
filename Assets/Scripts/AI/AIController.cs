08a435aa83d3d7ffe7242ab6c1770596b0c8d60b
Tobias Stroedicke <tobis-stroedicke@gmx.de>
2021-08-14 18:11:44 +0200

    Create AI Class
    Add Rotation

---
+++ Assets/Scripts/AI/AIController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour
+{
+    [Tooltip("Ship to Control")]
+    [SerializeField]
+    private AShip m_Ship;
+
+    [Range(-1, 1)]
+    [SerializeField]
+    public float m_RotateDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (m_Ship == null)
+            Debug.LogError($"{nameof(m_Ship)} is null!");
+
+        //m_Ship.RotateShip(100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DoRotation();
+    }
+
+    private void DoRotation()
+    {
+        Transform LookAtEnemy = PlayerContr.Get.ControlledShip.gameObject.transform;  // Who is the enemy?
+
+        //  If you find the enemy, do this:
+        var rotate = Quaternion.LookRotation(LookAtEnemy.position - m_Ship.transform.position);  //  This is the amount to rotate
+        float rotationLeft = rotate.eulerAngles.y - m_Ship.transform.rotation.eulerAngles.y;
+
+        if (rotationLeft <= 5
+            && rotationLeft >= -5)
+            m_Ship.RotateShip(0);
+        else
+        {
+            m_Ship.RotateShip(Mathf.Clamp(rotationLeft, -1, 1));
+        }
+
+    }
+
+
+
+    private PlayerContr GetPlayer()
+    {
+        return PlayerContr.Get;
+    }
+}
